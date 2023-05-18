9102cfe312209dc7a5ddcfe3edd896c1f48880e4
MikeMartemianov <127594151+MikeMartemianov@users.noreply.github.com>
2023-05-18 16:06:20 +0300

    CreateNewProgrammingLanguage.py
    My laboratory for create new programing language.

---
+++ CreateNewProgrammingLanguage.py
@@ -0,0 +1,53 @@
+try:
+
+	class CreateProgramingLang:		def __init__(self):
+
+			pass
+
+		class tokens:
+
+			def __init__(self):
+
+				self.tokens1 = []
+
+			def append_token(self, name:str, program:any):
+
+				self.tokens1.append([name, program])
+
+			def return_tokens(self):
+
+				return self.tokens1
+
+		class program:
+
+			def __init__(self, tokens):
+
+				self.tokens = tokens
+
+			def out(self, num:int):
+
+				return self.co[num]
+
+			def run(self, code:str):
+
+				code = code.split("\n")
+
+				for cod1 in code:
+
+					cod = cod1.split(" ")
+
+					n = 0
+
+					for self.co in cod:
+
+						for hng in self.tokens:
+
+							if self.co == hng[0]:
+
+								hng[1](cod, n)
+
+						n += 1
+
+except Exception:
+
+	pass
